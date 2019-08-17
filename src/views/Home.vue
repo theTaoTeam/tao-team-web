e193256975c7d4ff73a3fc786fb7e174ec27eb25
Weston Harper <wes@tao.team>
2019-08-17 14:00:24 -0600

    feat: add initial landing copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home-container">
+    <img src="../assets/logo.png" alt="Tao Team Logo" />
+    <p>
+      We're in the process of making this site awesome. In the meantime you can always reach out to us at
+      <a
+        href="mailto:info@tao.team"
+        target="_blank"
+      >info@tao.team</a>.
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {}
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 80%;
+  margin-bottom: 2vh;
+}
+
+p {
+  font-size: 3vh;
+  font-weight: bold;
+  width: 80%;
+  margin: 0 auto;
+}
+
+a {
+  text-decoration: none;
+  color: #ff5252;
+}
+</style>
