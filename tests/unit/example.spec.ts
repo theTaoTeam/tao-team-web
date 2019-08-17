104a5cc1d2cb5bff392e9559ae7ebf5b4715503d
Weston Harper <wes@tao.team>
2019-08-17 15:05:18 -0600

    fix: update config and example test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,11 @@
 import { shallowMount } from '@vue/test-utils';
-import Home from '@/components/Home.vue';
+import Home from '@/views/Home.vue';
 
 describe('Home.vue', () => {
   it('renders props.msg when passed', () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.text()).toMatch('We\'re in the process of making this site awesome.');
+    expect(wrapper.text()).toMatch(
+      'We\'re in the process of making this site awesome.',
+    );
   });
 });
