e746cecf4772943cd5d4ae94627ca3051ca91c41
Wes Harper <wes@tao.team>
2019-08-17 18:12:00 -0600

    Feature/circle ci (#1)

---
+++ tests/e2e/_eslintrc.js
@@ -0,0 +1,16 @@
+<% _ if (hasESLint) {
+_ %>
+  module.exports = {
+    plugins: [
+      'cypress'
+    ],
+      env: {
+      mocha: true,
+        'cypress/globals': true
+    },
+    rules: {
+      strict: 'off'
+    }
+  }
+<% _
+} _ %>
