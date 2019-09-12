28988cba438eb03e7445518fbe9ac74ee57cdaa1
Weston Harper <wes@tao.team>
2019-09-11 20:00:42 -0600

    fix(spec): fix unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -4,8 +4,6 @@ import Home from '@/views/Home.vue';
 describe('Home.vue', () => {
   it('renders props.msg when passed', () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.text()).toMatch(
-      'We\'re in the process of making this site awesome.',
-    );
+    expect(wrapper.text()).toMatch('Creative technology is kind of our jam.');
   });
 });
